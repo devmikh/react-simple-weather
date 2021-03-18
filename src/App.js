441afcec644013b3d41f0a4c06235d12488f9fbf
Mikhail Serebriakov <mishacyb@gmail.com>
2021-03-18 15:59:37 -0700

    Create Header component. Import it into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-      <h1>simple weather</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
